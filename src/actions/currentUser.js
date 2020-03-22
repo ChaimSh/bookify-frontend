01277832d2d76d2c1f7d283356224899436486e0
Kirill Shcherbina <rabbichaim770@gmail.com>
2020-03-22 15:59:18 -0500

    cookies

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -33,6 +33,7 @@ export const login = credentials => {
 export const getCurrentUser = () => {
     return dispatch => {
         return fetch("http://localhost:3000/api/v1/get_current_user", {
+            credentials: "include",
             method: "GET",
             headers: {
                "Content-Type": "application/json"
