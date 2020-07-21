f01c9a7a580457a3dfdefa2beeb25fef497f74b0
Kirill Shcherbina <rabbichaim770@gmail.com>
2020-07-20 22:13:32 -0500

    css changes to bookcard

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import { connect } from 'react-redux'
+import './BookCard.css'
 
 const BookCard = ({ book }) => {
 
@@ -8,12 +9,18 @@ console.log(book)
 
   return (
         book ?
-    <div>  
-        {console.log("Hey im in /components/Bookcard.js",book)}
+    <div className="card">  
+        {console.log("Hey im in /components/Bookcard.js",book)} 
+    <div className="container">
     <h3> {book.title} </h3>
-         <b>Description:</b> {book.description}<br/>
-         <b>Award:</b> {book.award}<br/>
-         <Link to={`/books/${book.id}/edit`}>Edit This Book</Link>
+     <br/>
+        <button>Upload Image</button>
+    <br/>
+        <h4> <b>Description:</b> {book.description}<br/> </h4>
+        <h4>  <b>Award:</b> {book.award}<br/> </h4>
+         <h5><Link to={`/books/${book.id}/edit`}>Edit This Book</Link> </h5>
+
+    </div>
     </div> 
     :
     <p>No book Here!</p>
