fe88530b0190cdb49c4e2c6a652ece7aed565194
Kirill Shcherbina <rabbichaim770@gmail.com>
2020-07-28 22:54:03 -0500

    small edit to background and navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { NavLink } from 'react-router-dom'
 import Logout from './Logout'
-
+import './NavBar.css';
 
 
 const NavBar = ({currentUser, loggedIn}) => {
